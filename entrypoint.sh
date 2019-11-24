e13badb9feb856e63d038a40b6f04d6086422738
Rojan <rojan@securifyinc.com>
2019-11-23 19:53:44 -0800

    Copy checked out codes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 function find_files(){
-  files=$(git diff --name-only --diff-filter=AM HEAD~1..HEAD /github/workspace)
+  files=$(git diff --name-only --diff-filter=AM HEAD~1..HEAD /checkcodes)
 }
 
 find_files
-python3 /analyze.py --files $files --repoloc /github/workspace
+python3 /analyze.py --files $files --repoloc /checkcodes
