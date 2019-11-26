1a0439530474a11a6c13ff94d7245753ed4ad848
Rojan <rojan@securifyinc.com>
2019-11-25 17:13:09 -0800

    Fixed entrypoint error

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,8 @@
 
 function find_files(){
   files=$(git diff --name-only --diff-filter=AM HEAD~1..HEAD /github/workspace)
+  repoName=$(cd /github/workspace && git remote get-url origin)
 }
 
 find_files
-python3 /analyze.py --files $files --repoloc /github/workspace
+python3 /analyze.py --files $files --repoloc /github/workspace --repoName $repoName
