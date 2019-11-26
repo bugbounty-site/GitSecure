f4bd8b92427387c7c94572a676504c30334d5eb3
Rojan <rojan@securifyinc.com>
2019-11-25 17:11:12 -0800

    Add proper storing schema for results

--- codes/analyze.py
+++ codes/analyze.py
@@ -3,6 +3,17 @@ import requests
 from itertools import product
 
 regex = {}
+results = {"results":[]}
+global args
+def sendMessage():
+	if len(results['results']) > 0:
+		print(results)
+
+def result(fileResult):
+	for res in fileResult:
+		if res is not None:
+			results['results'].append(res)
+
 def getRegex():
 	global regex
 	regex = requests.get('https://raw.githubusercontent.com/dxa4481/truffleHogRegexes/master/truffleHogRegexes/regexes.json').json()
@@ -24,24 +35,31 @@ def matchRegex(line):
 	return matchedArray
 
 def readFile(file):
-	f = open('{dirs}/{file}'.format(dirs = directory, file = file), encoding='utf-8')
+	fileReturn = {file:[]}
+	f = open('{dirs}/{file}'.format(dirs = args.dirs, file = file), encoding='utf-8')
 	lines = f.readlines()
 	for index, line in enumerate(lines):
 		matched = matchRegex(line.strip())
 		if len(matched) > 0:
-			print('Found {matches} in line {lineNum} of {fileName}'.format(matches = ','.join(matched), lineNum = index+1, fileName = file))
-
+			#matchFound = {'{linenum}'.format(linenum=index+1):matched}
+			#fileReturn[file].append(matchFound)
+			found = 'Found {matches} in line {lineNum}'.format(matches = ','.join(matched), lineNum = index+1, fileName = file)
+			fileReturn[file].append(found)
+	if len(fileReturn[file]) > 0:
+		return fileReturn
+	return None
 
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--files', dest='filesToCheck', nargs="+") # adds all files passed into an array. The files are retrieved from git diff
 	parser.add_argument('--repoloc', dest='dirs') # gets argument from repoloc and assigns it to a string.
+	parser.add_argument('--repoName', dest='name')
 	args = parser.parse_args()
-	global directory
-	directory = args.dirs
+	print('Analysing {repoName}'.format(repoName=args.name))
 	getRegex()
 	pool = multiprocessing.Pool(10)
-	runPool = pool.map_async(readFile, args.filesToCheck)
-	runPool.get()
+	x = pool.map_async(readFile, args.filesToCheck, callback=result) # this will run the scans on files and call result for each result
+	x.get()
 	pool.close()
 	pool.join()
+	sendMessage()
